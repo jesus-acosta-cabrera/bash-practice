175a39600a46740ba4dbc668f11c556fad064b70
Jesus Acosta <jesusacosta@Jesuss-MacBook-Air.local>
2025-07-22 17:34:21 -0400

    data: added validation to inputs from bash while asking for age and name

--- bash-examples/input-output.sh
+++ bash-examples/input-output.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
+empty="false"
+
 # Asking for user name
 
-echo -n "What's your name? "
-read name
-
+while [ "$empty" == "false" ];
+do
+  echo -n "What's your name? "
+  read name
+  if [ -z "${name}" ]; then
+    echo -e "Please, write something. \nPress enter to continue..."
+    read
+    clear
+  else
+    empty="true"
+  fi
+done
 # Asking for user age
+empty="false"
+re='^[0-9]+$'
 
-echo -n "How old are you? "
-read age
+while [ "$empty" == "false" ];
+do
+  echo -n "How old are you? "
+  read age
+  if [ -z "${age}" ]; then
+    echo -e "Please, write something. \nPress enter to continue..."
+    read
+    clear
+  elif ! [[ $age =~ $re ]]; 
+  then
+    echo -e "Please, introduce a number. \nPress enter to continue..."
+    read
+    clear
+  else
+    empty="true"
+  fi
+done
 
 # Displaying when user name and when it was displayed
 echo "Your name is: $name"
