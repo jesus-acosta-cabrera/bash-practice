7f31fef55450d63a2479c4b5ba8bc45fe6b95ba2
Jesus Acosta <jesusacosta@Jesuss-MacBook-Air.local>
2025-07-22 17:12:47 -0400

    added gitignore and base input-output example for a person's name and age

---
+++ bash-examples/input-output.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Asking for user name
+
+echo -n "What's your name? "
+read name
+
+# Asking for user age
+
+echo -n "How old are you? "
+read age
+
+# Displaying when user name and when it was displayed
+echo "Your name is: $name"
+echo "Your age is: $age"
